c2939599141687cd541c3960fef30a23960df7fa
calcado <pecalcado@gmail.com>
2022-12-13 15:30:41 -0300

    CRUD rentals

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -11,7 +11,7 @@ games.name AS "gamesName" , games."categoryId", games."categoryName", categories
  FROM rentals 
  JOIN customers ON customers.id="customersId" 
  JOIN games ON games.id="gamesId", 
- JOIN categories ON categories.id="categoriesId" WHERE customer.id=$1;`)
+ JOIN categories ON categories.id="categoriesId" WHERE customer.id=$1;`,[customerId])
 
 const results = rentalsCustomer.map((result)=>  {
   return {
@@ -43,7 +43,7 @@ games.name AS "gamesName" , games."categoryId", games."categoryName", categories
  FROM rentals 
  JOIN customers ON customers.id="customersId" 
  JOIN games ON games.id="gamesId", 
- JOIN categories ON categories.id="categoriesId" WHERE game.id=$1;`)
+ JOIN categories ON categories.id="categoriesId" WHERE game.id=$1;`,[gameId])
 
 const results = rentalsGame.map((result)=>  {
   return {
@@ -108,7 +108,7 @@ try {
 export async function postRentals(req,res){
 const {customerId,gameId,daysRented} = res.locals.rental
 
-const rentDate = dayjs().format("YYYY/MM/DD");
+const rentDate = dayjs().format("YYYY-MM-DD");
 const returnDate = null
 const delayFee = null
 
@@ -129,10 +129,31 @@ res.sendStatus(201)
 
 export async function postReturnRentals(req,res){
 const {id} = req.params
-const returnDate = dayjs().format("YYYY/MM/DD");
-
+const deliverDate = dayjs().format("YYYY-MM-DD");
+const delayFee = null
+if(!id){
+  return res.send(404)
+}
 
 try{
+  const rent = await connection.query(`SELECT * FROM rentals WHERE id=${id}`)
+  const maxDate = new Date(rent.rentDate)
+  const returnDate = new Date(deliverDate)
+  
+  
+  if(deliverDate.isAfter(maxDate)){
+    const difference  = Math.abs(returnDate - maxDate);
+    const minutes = 1000*60*60*24;
+    const daysDelay = difference/minutes
+
+    const game = await connection.query(`SELECT * FROM games WHERE id = $1;`,[id])
+    const pricePerDay = game.pricePerDay
+    const delayFee = pricePerDay*daysDelay 
+    return delayFee
+  }
+
+ 
+
 await connection.query(`UPDATE rentals (returnDate, delayFee) VALUES ($1,$2) WHERE id=${id};`, [returnDate, delayFee])
  res.sendStatus(200) 
 }catch(err){console.log(err)}
@@ -140,19 +161,18 @@ await connection.query(`UPDATE rentals (returnDate, delayFee) VALUES ($1,$2) WHE
 
 export async function deleteRentals(req,res){
   const {id} = req.params
-  if(!id){
-    return res.send(404)
-  }
+  
   try{
   const rental =  await connection.query(`
   SELECT rentals.*,customers.id,customers.name,
   FROM rentals JOIN customers WHERE id=${id};`)
-  if(rental.returnDate){
+  if(rental.returnDate!==null){
     return res.send(400)
   }
   }catch(err){console.log(err)}
+  
   try{
-    await connection.query(`DELETE * FROM rentals WHERE id = ${id};`)
+    await connection.query(`DELETE * FROM rentals WHERE id = $1;`,[id])
     res.send(200)
   }catch(err){
     console.log(err)}
